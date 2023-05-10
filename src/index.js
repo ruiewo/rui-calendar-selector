45e1885fd499dfb03dd06b63b5ea244dcca8ab01
ruiewo <109674512+ruiewo@users.noreply.github.com>
2023-05-11 00:34:27 +0900

    refactoring.

--- src/index.js
+++ src/index.js
@@ -1,108 +1,96 @@
-console.log('-----------rui-calendar-selector start-----------')
-
-async function main() {
-  await render(true)
-}
-
-function getCalendars() {
-  const [myList, otherList] = document.querySelectorAll(`[role="list"]`)
-  return [...myList.children, ...otherList.children]
-}
-
-function updateCalendarState(list = []) {
-  for (const dom of getCalendars()) {
-    setState(dom, list.includes(dom.querySelector('span').textContent))
+const ui = (() => {
+  function getCalendars() {
+    const [myList, otherList] = document.querySelectorAll(`[role="list"]`)
+    return [...myList.children, ...otherList.children]
   }
-}
 
-function getActiveCalendars() {
-  const activeList = []
-  for (const cal of getCalendars()) {
-    if (cal.querySelector('input').checked) {
-      activeList.push(cal.querySelector('span').textContent)
+  function updateCalendarState(activeList = []) {
+    for (const dom of getCalendars()) {
+      const state = activeList.includes(dom.querySelector('span').textContent)
+      const checkbox = dom.querySelector('input')
+      if (state !== checkbox.checked) {
+        checkbox.click()
+      }
     }
   }
 
-  return activeList
-}
+  function getActiveCalendars() {
+    const activeList = []
+    for (const cal of getCalendars()) {
+      if (cal.querySelector('input').checked) {
+        activeList.push(cal.querySelector('span').textContent)
+      }
+    }
 
-function setState(calendar, state) {
-  const checkbox = calendar.querySelector('input')
-  if (state !== checkbox.checked) {
-    checkbox.click()
+    return activeList
   }
-}
 
-async function render(isInit = false) {
-  const groups = await storage.getGroups()
+  async function addGroup() {
+    const groupName = window.prompt('Please enter a group name.')
+    if (groupName) {
+      await storage.addGroup(groupName, getActiveCalendars())
+    }
+    await render()
+  }
 
-  const ul = document.createElement('ul')
-  ul.classList.add('rui-groups')
-  for (const { groupName, list } of groups) {
+  async function deleteGroup(groupName) {
+    await storage.deleteGroup(groupName, getActiveCalendars())
+    await render()
+  }
+
+  async function render(isInit = false) {
+    const groups = await storage.getGroups()
+
+    const ul = document.createElement('ul')
+    ul.classList.add('rui-groups')
+    for (const { groupName, list } of groups) {
+      const li = document.createElement('li')
+
+      const span = document.createElement('span')
+      span.textContent = groupName
+      span.onclick = () => updateCalendarState(list)
+
+      const button = document.createElement('div')
+      button.classList.add('deleteButton')
+      button.onclick = () => deleteGroup(groupName)
+
+      li.appendChild(span)
+      li.appendChild(button)
+
+      ul.appendChild(li)
+    }
+
     const li = document.createElement('li')
-
-    const span = document.createElement('span')
-    span.textContent = groupName
-    span.onclick = () => updateCalendarState(list)
-
-    const button = document.createElement('div')
-    button.classList.add('deleteButton')
-    button.onclick = () => deleteGroup(groupName)
-
-    li.appendChild(span)
-    li.appendChild(button)
-
+    li.classList.add('addRow')
+    const addButton = document.createElement('div')
+    addButton.classList.add('addButton')
+    addButton.textContent = '+Add'
+    addButton.onclick = addGroup
+    li.appendChild(addButton)
     ul.appendChild(li)
+
+    const header = document.createElement('div')
+    header.textContent = `Calendar Group`
+    header.classList.add('rui-header')
+
+    if (isInit) {
+      const wrapper = document.createElement('div')
+      wrapper.id = 'rui-calendar-selector'
+      wrapper.appendChild(header)
+      wrapper.appendChild(ul)
+
+      const calendarDiv = document.querySelector('[jsname="Hostde"]').parentElement
+      calendarDiv.parentNode.insertBefore(wrapper, calendarDiv)
+    } else {
+      const wrapper = document.getElementById('rui-calendar-selector')
+      wrapper.replaceChildren()
+      wrapper.appendChild(header)
+      wrapper.appendChild(ul)
+    }
   }
-  const addButton = document.createElement('li')
-  addButton.classList.add('addButton')
-  addButton.textContent = 'save as'
-  addButton.onclick = addGroup
-  ul.appendChild(addButton)
 
-  const header = document.createElement('div')
-  header.textContent = `Calendar Group`
-  header.classList.add('rui-header')
-
-  let wrapper
-  if (isInit) {
-    wrapper = document.createElement('div')
-    wrapper.id = 'rui-calendar-selector'
-  } else {
-    wrapper = document.getElementById('rui-calendar-selector')
-    wrapper.replaceChildren()
-  }
-  wrapper.appendChild(header)
-  wrapper.appendChild(ul)
-
-  if (isInit) {
-    const wrapper = document.createElement('div')
-    wrapper.id = 'rui-calendar-selector'
-    wrapper.appendChild(header)
-    wrapper.appendChild(ul)
-
-    const calendarDiv = document.querySelector('[jsname="Hostde"]').parentElement
-    calendarDiv.parentNode.insertBefore(wrapper, calendarDiv)
-  } else {
-    const wrapper = document.getElementById('rui-calendar-selector')
-    wrapper.replaceChildren()
-    wrapper.appendChild(header)
-    wrapper.appendChild(ul)
-  }
-}
-
-async function addGroup() {
-  const groupName = window.prompt('Please enter a group name.')
-  if (groupName) {
-    await storage.addGroup(groupName, getActiveCalendars())
-  }
-  await render()
-}
-
-async function deleteGroup(groupName) {
-  await storage.deleteGroup(groupName, getActiveCalendars())
-  await render()
-}
+  return { render }
+})()
 
 const storage = (() => {
   const calendarGroupKey = 'calendarGroupKey'
@@ -134,5 +122,9 @@ const storage = (() => {
     deleteGroup,
   }
 })()
+
+async function main() {
+  await ui.render(true)
+}
 
 main()
